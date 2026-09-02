5f9225373aa5ed400373d4e810946af1ab876724
AsterZ <80435476+AsterZ99@users.noreply.github.com>
2026-09-02 17:46:55 +0800

    Add Install.cmd

---
+++ Install.cmd
@@ -0,0 +1,10 @@
+@echo off
+chcp 65001 >nul
+net session >nul 2>&1
+if errorlevel 1 (
+  powershell.exe -NoProfile -Command "Start-Process -FilePath '%~f0' -Verb RunAs"
+  exit /b
+)
+powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0Install.ps1"
+echo.
+pause
